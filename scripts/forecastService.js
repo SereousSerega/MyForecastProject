b59d228516a91a5bc1b61cb2a04983caf476d75d
SereousSerega <sereousserega@gmail.com>
2024-09-14 17:25:46 +0300

    Forecast Service ready

--- scripts/forecastService.js
+++ scripts/forecastService.js
@@ -1,19 +1,29 @@
+import { forecast } from './forecast.js';
+
 class ForecastService {
     #url = "https://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f"
     #APIKey = 'c570d99a7b10772384d733cb0c7e08d5'
 
-    codes = {
-        Kyiv: 703448,
-        London: 2643743,
-        NewYork: 5128638,
-    }
+    codes = [
+        703448,
+        2643743,
+        5128638,
+    ]
+    
     constructor() {
-        this.requestWeather()
+        
+        this.codes.forEach(code => {
+            this.requestWeather(code);
+        });
+
+        // this.requestWeather(703448);
+        
+        
     }
 
-    requestWeather() {
+    requestWeather(code) {
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?id=703448&appid=${this.#APIKey}`
+        let url = `https://api.openweathermap.org/data/2.5/weather?id=${code}&appid=${this.#APIKey}`
 
         fetch(url)
             .then((request) => {
@@ -28,6 +38,10 @@ class ForecastService {
             })
             .then((json) => {
                 console.log(json)
+
+                let f = json
+
+                forecast.createItem(f.name, f.main.temp, f.weather[0].description, `https://openweathermap.org/img/wn/${f.weather[0].icon}.png`)
             })
             .catch((err)=>{
                 console.warn(err.message)
