f338d85667ffc5e19d01210f79cf32e093a88bcd
SereousSerega <sereousserega@gmail.com>
2024-09-14 22:13:58 +0300

    Finished project

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,8 +1,13 @@
+import { forecastService } from './forecastService.js'
+
 class Forecast {
     container
     directory
     items = [];
+    weekItem
     isOpen = true;
+    metric = '°C'
+    metricBtn
 
     structure = {
         container: `
@@ -38,7 +43,7 @@ class Forecast {
         <div class="forecast_list">
             <div class="f_24">${v1}</div>
             <div>
-                <span>${v2}</span> <br>
+                <span>${v2} ${this.metric}</span> <br>
                 <span>${v3}</span>
             </div>
             <div><img src="${v4}" alt="weather"></div>
@@ -49,8 +54,8 @@ class Forecast {
             <header header class= "forecast_header roboto-bold f_24" > OpenWeather Forecast</header>
         `,
 
-        footer: `
-        <footer footer class = "forecast_footer roboto-bold f_24" ></footer>
+        footer: () => `
+        <footer class = "forecast_footer roboto-bold f_24" ><button class="metric-toggle-btn"></button></footer>
     </div >
     `,
 
@@ -68,14 +73,24 @@ class Forecast {
                 <div></div>
             </div >
     `,
+
+        weekItem: (date, temp, condition, icon,) => `
+            <tr>
+                <td>${date}</td>
+                <td>${temp} ${this.metric}</td>
+                <td>${condition}</td>
+                <td><img src="${icon}" alt="weather"></td>
+            </tr>
+        `
     }
 
     constructor() {
-        // document.body.insertAdjacentHTML("beforeend", this.structure.container)
         this.createForecast()
+        forecastService.getAllWeatherData();
     }
 
     createForecast() {
+
         this.container = document.createElement('div');
         this.container.id = "forecastContainer"
         this.container.className = "forecast_container roboto-regular"
@@ -84,14 +99,15 @@ class Forecast {
 
         this.directory = document.createElement('div');
         this.directory.className = 'forecast_directory'
-        
+
         this.container.append(this.directory)
 
 
-        // this.directory.insertAdjacentHTML('beforeend', this.structure.loading);
-        // this.directory.insertAdjacentHTML('beforeend', "<h2>Loading...</h2>")
 
-        this.container.insertAdjacentHTML('beforeend', this.structure.footer)
+        this.container.insertAdjacentHTML('beforeend', this.structure.footer())
+
+        this.metricBtn = this.container.querySelector('.metric-toggle-btn');
+        this.metricBtn.addEventListener('click', () => this.toggleMetric());
 
         document.body.append(this.container)
         console.log(this.container)
@@ -99,9 +115,82 @@ class Forecast {
     }
 
     createItem(v1, v2, v3, v4) {
-        const itemHtml = this.structure.item(v1, v2, v3, v4);
-        this.directory.insertAdjacentHTML('beforeend', itemHtml);
+
+        if (this.metric == '°F') {
+            v2 = this.convertToFahrenheit(v2)
+        }
+
+        const itemHtml = document.createElement('div');
+
+        itemHtml.innerHTML = this.structure.item(v1, v2, v3, v4);
+
+        itemHtml.addEventListener('click', () => this.createWeekItem(v1));
+
+        this.directory.appendChild(itemHtml);
+
         this.items.push(itemHtml);
+    }
+
+    async createWeekItem(city) {
+        this.hideItems()
+        console.warn(city)
+        
+        let itemHtml = document.createElement('div')
+        itemHtml.className = 'forecast_week'
+        itemHtml.innerHTML += `<h2>${city}</h2>`
+        let itemTable = document.createElement('table')
+        itemHtml.append(itemTable)
+        let itemTbody = document.createElement('tbody')
+        itemTable.append(itemTbody)
+
+        itemHtml.addEventListener('click', () => this.showItems());
+
+        await forecastService.requestWeek(city)
+            .then((obj) => {
+                
+
+                obj.forEach(item => {
+
+                    if (this.metric == '°F') {
+                        item.temp = this.convertToFahrenheit(item.temp)
+                    }
+                    itemTbody.innerHTML += this.structure.weekItem(item.date, item.temp, item.description, `https://openweathermap.org/img/wn/${item.icon}.png`);
+                });
+            })
+
+
+        this.weekItem = itemHtml
+        this.directory.appendChild(itemHtml);
+
+
+    }
+
+    hideItems() {
+        this.items.forEach(item => {
+            console.log(item)
+            item.remove()
+        });
+    }
+
+    showItems() {
+        if (this.weekItem) {
+            this.weekItem.remove()
+        }
+        forecastService.getAllWeatherData()
+    }
+    toggleMetric() {
+        if (this.metric === '°C') {
+            this.metric = '°F';
+        } else {
+            this.metric = '°C';
+        }
+        
+        this.hideItems()
+        this.showItems()
+    }
+    convertToFahrenheit(celsius) {
+        let c = (celsius * 9/5) + 32;
+        return parseFloat(c.toFixed(1));
     }
 }
 
--- scripts/forecastService.js
+++ scripts/forecastService.js
@@ -4,33 +4,44 @@ class ForecastService {
     #url = "https://api.openweathermap.org/data/2.5/weather?id=703448&appid=bf35cac91880cb98375230fb443a116f"
     #APIKey = 'c570d99a7b10772384d733cb0c7e08d5'
 
+    cities = {
+        Kyiv: 703448,
+        London: 2643743,
+        NewYork: 5128638,
+    }
+
     codes = [
         703448,
         2643743,
         5128638,
+        698740,
+        756135,
+        2950159,
+        3128760
     ]
-    
-    constructor() {
-        
-        this.codes.forEach(code => {
-            this.requestWeather(code);
-        });
 
-        // this.requestWeather(703448);
-        
-        
+    constructor() {
+
+        // this.getAllWeatherData();
+
     }
 
-    requestWeather(code) {
+    async getAllWeatherData() {
+        for (let code of this.codes) {
+            await this.requestWeather(code);
+        }
+    }
 
-        let url = `https://api.openweathermap.org/data/2.5/weather?id=${code}&appid=${this.#APIKey}`
+    async requestWeather(code) {
 
-        fetch(url)
+        let url = `https://api.openweathermap.org/data/2.5/weather?id=${code}&lang=uk&units=metric&appid=${this.#APIKey}`
+
+        await fetch(url)
             .then((request) => {
                 console.log(request)
                 if (!request.ok) {
-                    throw new Error("Da suka!");
-                    
+                    throw new Error(request.status);
+
                 }
 
                 return request.json()
@@ -43,10 +54,54 @@ class ForecastService {
 
                 forecast.createItem(f.name, f.main.temp, f.weather[0].description, `https://openweathermap.org/img/wn/${f.weather[0].icon}.png`)
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.warn(err.message)
             })
     }
+
+    async requestWeek(city) {
+
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=uk&units=metric&appid=c570d99a7b10772384d733cb0c7e08d5`
+        let forecast = await fetch(url)
+            .then((request) => {
+                console.log(request)
+                if (!request.ok) {
+                    throw new Error(request.status);
+
+                }
+
+                return request.json()
+            })
+            .then((json) => {
+
+                return json
+            })
+            .catch((err) => {
+                console.warn(err.message)
+            })
+
+        const filteredData = forecast.list.filter(item => {
+            const time = item.dt_txt.split(' ')[1];
+            return time === '15:00:00'
+        }).map(item => ({
+            date: item.dt_txt.split(" ")[0],
+            temp: item.main.temp,
+            feels_like: item.main.feels_like,
+            temp_min: item.main.temp_min,
+            temp_max: item.main.temp_max,
+            pressure: item.main.pressure,
+            humidity: item.main.humidity,
+            description: item.weather[0].description,
+            icon: item.weather[0].icon,
+            wind_speed: item.wind.speed
+
+        }));
+
+        return filteredData
+        console.log(filteredData);
+
+    }
+
 }
 
 const forecastService = new ForecastService()
